4a7344e824eb39f0e361f447f41ffab79f0f5c53
Juan García <43949658+garcia-s@users.noreply.github.com>
2021-10-19 07:55:29 -0500

    create the pupa app

---
+++ pupaadmin/android/app/src/main/kotlin/com/example/pupaadmin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pupaadmin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pupaapp/android/app/src/main/kotlin/com/example/pupaapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pupaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
